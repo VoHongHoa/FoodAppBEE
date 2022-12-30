826cea01a7c148080e379867db4df640b47bd1d3
VoHongHoa <19521522@gm.uit.edu.vn>
2022-12-30 14:03:05 +0700

    updated

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ mongoose.connection.once("open", () => {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
 
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-  logEvents(
-    `${err.no}:${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
-});
+// mongoose.connection.on("error", (err) => {
+//   console.log(err);
+//   logEvents(
+//     `${err.no}:${err.code}\t${err.syscall}\t${err.hostname}`,
+//     "mongoErrLog.log"
+//   );
+// });
